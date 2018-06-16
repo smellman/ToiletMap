206fffe7ff052b7c4bfdd702a742c433a48415fb
Taro Matsuzawa <taro@georepublic.co.jp>
2018-06-16 16:06:29 +0900

    地図の表示まで対応

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// 1: MapViewをインポート
+import { MapView } from 'expo';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        { /* 2: MapViewを配置 */ }
+        <MapView
+          style={styles.mapview}
+          initialRegion={{
+            latitude: 35.681262,
+            longitude: 139.766403,
+            latitudeDelta: 0.00922,
+            longitudeDelta: 0.00521,
+          }}>
+        </MapView>
       </View>
     );
   }
@@ -17,5 +28,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  // 3: MapViewに対応したスタイル
+  mapview: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
