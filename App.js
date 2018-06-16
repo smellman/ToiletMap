d0fe8a5438bf18743ecbf8c0f32af3753342f1f8
Taro Matsuzawa <taro@georepublic.co.jp>
2018-06-16 20:34:21 +0900

    トイレの位置の取得ができるまで作成

--- App.js
+++ App.js
@@ -1,16 +1,85 @@
 import React from 'react';
-// 1: TouchableOpacityを追加
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { MapView } from 'expo';
+// 1: turf で使う関数をインポート
+import {
+  point
+} from '@turf/helpers'
+import destination from '@turf/destination'
 
 export default class App extends React.Component {
-  // 2: fetchToilet関数を追加、まだ何もしない
-  fetchToilet = () => {
+  constructor(props) {
+    super(props)
+    this.state = {
+      elements: [], // 2: APIで取得した要素
+      // 3: bboxの指定
+      south: null,
+      west: null,
+      north: null,
+      east: null,
+    }
+  }
+
+  // 4: 地図の画面が変更されるたびにbboxを計算
+  onRegionChangeComplete = (region) => {
+    const center = point([region.longitude, region.latitude])
+    // 5: 111キロメートルから中心点から縦幅、横幅を計算
+    const verticalMeter = 111 * region.latitudeDelta / 2
+    const horizontalMeter = 111 * region.longitudeDelta / 2
+    // 6: 実際の距離を計算
+    const options = {units: 'kilometers'}
+    const south = destination(center, verticalMeter, 180, options)
+    const west = destination(center, horizontalMeter, -90, options)
+    const north = destination(center, verticalMeter, 0, options)
+    const east = destination(center, horizontalMeter, 90, options)
+    // 7: 計算結果(GeoJSON)からbboxを保存する
+    this.setState({
+      south: south.geometry.coordinates[1],
+      west: west.geometry.coordinates[0],
+      north: north.geometry.coordinates[1],
+      east: east.geometry.coordinates[0],
+    })
+  }
+
+  // 8: fetchToilet内でawaitを使うのでasyncに
+  fetchToilet = async () => {
+    const south = this.state.south
+    const west = this.state.west
+    const north = this.state.north
+    const east = this.state.east
+    // 9: テンプレートリテラルを使ってbboxを展開
+    const body = `
+    [out:json];
+    (
+      node
+        [amenity=toilets]
+        (${south},${west},${north},${east});
+      node
+        ["toilets:wheelchair"=yes]
+        (${south},${west},${north},${east});
+    );
+    out;
+    `
+    // 10: fetch関数に渡すoptionを指定
+    const options = {
+      method: 'POST',
+      body: body
+    }
+    // 11: fetch関数でOverpass APIのエントリポイントにアクセスし、取得したJSONを保存
+    try {
+      const response = await fetch('https://overpass-api.de/api/interpreter', options)
+      const json = await response.json()
+      this.setState({elements: json.elements})
+    } catch (e) {
+      console.log(e)
+    }
   }
   render() {
     return (
       <View style={styles.container}>
+        { /* 12: onRegionChangeCompleteを関連付け */ }
         <MapView
+          onRegionChangeComplete={this.onRegionChangeComplete}
           style={styles.mapview}
           initialRegion={{
             latitude: 35.681262,
@@ -18,15 +87,24 @@ export default class App extends React.Component {
             latitudeDelta: 0.00922,
             longitudeDelta: 0.00521,
           }}>
-          <MapView.Marker
-            coordinate={{
-              latitude: 35.681262,
-              longitude: 139.766403
-            }}
-            title="東京駅"
-          />
+          { /* 13: マーカーをstateのelementsから配置するようにする */}
+          {
+            this.state.elements.map((element) => {
+              let title = "トイレ"
+              if (element.tags["name"] !== undefined) {
+                title = element.tags["name"]
+              }
+              return (<MapView.Marker
+                coordinate={{
+                  latitude: element.lat,
+                  longitude: element.lon,
+                }}
+                title={title}
+                key={"id_" + element.id}
+              />)
+            })
+          }
         </MapView>
-        { /* 3: ボタンのコンテナとボタンを追加 */ }
         <View style={styles.buttonContainer}>
           <TouchableOpacity
             onPress={() => this.fetchToilet()}
@@ -45,12 +123,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-end', // 4: 画面下から並ぶように指定
+    justifyContent: 'flex-end',
   },
   mapview: {
     ...StyleSheet.absoluteFillObject,
   },
-  // 5: ボタンを配置するためのコンテナなどのスタイル
   buttonContainer: {
     flexDirection: 'row',
     marginVertical: 20,
