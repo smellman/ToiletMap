57e977fa73111c8b26a6ae06d59b059e7eee6eb4
Taro Matsuzawa <taro@georepublic.co.jp>
2018-06-16 17:45:48 +0900

    トイレ取得ボタンを追加

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// 1: TouchableOpacityを追加
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { MapView } from 'expo';
 
 export default class App extends React.Component {
+  // 2: fetchToilet関数を追加、まだ何もしない
+  fetchToilet = () => {
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -14,7 +18,6 @@ export default class App extends React.Component {
             latitudeDelta: 0.00922,
             longitudeDelta: 0.00521,
           }}>
-          { /* 1: Markerを配置 */ }
           <MapView.Marker
             coordinate={{
               latitude: 35.681262,
@@ -23,6 +26,15 @@ export default class App extends React.Component {
             title="東京駅"
           />
         </MapView>
+        { /* 3: ボタンのコンテナとボタンを追加 */ }
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.fetchToilet()}
+            style={styles.button}
+          >
+            <Text style={styles.buttonItem}>トイレ取得</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -33,9 +45,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end', // 4: 画面下から並ぶように指定
   },
   mapview: {
     ...StyleSheet.absoluteFillObject,
+  },
+  // 5: ボタンを配置するためのコンテナなどのスタイル
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+  },
+  button: {
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  buttonItem: {
+    textAlign: 'center'
   },
 });
